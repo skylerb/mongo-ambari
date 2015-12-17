1c17a4c36105a3338aeb877d0b591828d7532ebb
Skyler Beckman <sbeckman@outlook.com>
2015-12-17 09:58:34 -0500

    Updating to MongoDB 3.0

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -4,7 +4,7 @@ import os
 
 config = Script.get_config()
 
-db_path = default('configurations/mongodb/db_path', '/var/lib/mongo')
+db_path = default('configurations/mongodb/db_path', '/data/mongo')
 bind_ip = default('configurations/mongodb/bind_ip', '0.0.0.0')
 tcp_port = default('configurations/mongodb/tcp_port', '27017')
 # The web status page is always accessible at a port number that is 1000 greater than the port determined by tcp_port.
