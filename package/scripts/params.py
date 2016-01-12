51f003638234fd51211174ea3f64a33399d816a2
Skyler Beckman <skylerb@users.noreply.github.com>
2016-01-12 12:37:00 -0500

    Update params.py

--- package/scripts/params.py
+++ package/scripts/params.py
@@ -7,7 +7,7 @@ config = Script.get_config()
 db_path = default('configurations/mongodb/db_path', '/data/mongo')
 bind_ip = default('configurations/mongodb/bind_ip', '0.0.0.0')
 tcp_port = default('configurations/mongodb/tcp_port', '27017')
-log_path = defualt('configurations/mongodb/log_path', '/var/log/mongodb/mongod.log')
+log_path = default('configurations/mongodb/log_path', '/var/log/mongodb/mongod.log')
 db_auth = default('configurations/mongodb/db_auth', 'false')
 # The web status page is always accessible at a port number that is 1000 greater than the port determined by tcp_port.
 mongo_host = default('configurations/mongodb/mongo_host', 'unknown')
