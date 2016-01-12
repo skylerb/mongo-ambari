a7ee67f50aca3d92e043a4f41daa988cb3131d46
Skyler Beckman <skylerb@users.noreply.github.com>
2016-01-12 14:20:34 -0500

    Update mongo_base.py

--- package/scripts/mongo_base.py
+++ package/scripts/mongo_base.py
@@ -13,7 +13,10 @@ class MongoBase(Script):
         env.set_params(params)
 
         self.install_packages(env)
-
+        self.create_linux_user(params.mongo_user, params.mongo_group)
+  	    if params.mongo_user != 'root':
+  		    Execute('cp /etc/sudoers /etc/sudoers.bak')
+  		    Execute('echo "'+params.mongo_user+' ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers')
         if os.path.exists(self.repos_file_path):
             print "File exists"
         else:
@@ -26,13 +29,23 @@ class MongoBase(Script):
         if self.mongo_packages is not None and len(self.mongo_packages):
             for pack in self.mongo_packages:
                 Package(pack)
+                
+    def create_linux_user(self, user, group):
+	  try: pwd.getpwnam(user)
+	  except KeyError: Execute('adduser ' + user)
+	  try: grp.getgrnam(group)
+	  except KeyError: Execute('groupadd ' + group)
 
     def configureMongo(self, env):
         import params
 
         env.set_params(params)
         if not os.path.exist(params.db_path):
-            Execute('mkdir -p ' + params.db_path)
+            Directory([params.db_path],
+          	  owner=params.mongo_user,
+          	  group=params.mongo_group,
+          	  recursive=False
+          	  )
         File(self.config_file_path,
              content=Template("mongod.conf.j2"),
              mode=0644
